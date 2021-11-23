8f7ff68c960106fc6f61bffcec48af033a09af4a
aafksab <36931057+aafksab@users.noreply.github.com>
2021-11-23 15:25:44 -0700

    added args parser and loop to print number of args

--- get-quote.py
+++ get-quote.py
@@ -1,16 +1,24 @@
 import random
+import argparse
+parser = argparse.ArgumentParser(description='Number of quotes to return')
+parser.add_argument('--number', dest='number', type=int, help='Number of quotes to return')
+args = parser.parse_args()
+number = args.number
 
-def primary():
+def primary(number):
   f = open("quotes.txt")
   quotes = f.readlines()
   f.close()
   last = len(quotes) -1
 
-  rnd = random.randint(0,last)
-  print(quotes[rnd], end="")
+  if number <= 1:
+    rnd = random.randint(0,last)
+    print(quotes[rnd], end="")
 
-  rnd = random.randint(0,last)
-  print(quotes[rnd], end="")
+  if number > 1:
+    for n in range(number):
+      rnd = random.randint(0,last)
+      print(quotes[rnd], end="")
 
 if __name__== "__main__":
-  primary()
+  primary(number)
