21786151add4c93be6b2e2fd8b098896a6ed5f45
aafksab <36931057+aafksab@users.noreply.github.com>
2021-11-23 14:56:51 -0700

    added more then one quote

--- get-quote.py
+++ get-quote.py
@@ -5,8 +5,12 @@ def primary():
   quotes = f.readlines()
   f.close()
   last = len(quotes) -1
+
   rnd = random.randint(0,last)
   print(quotes[rnd])
 
+  rnd = random.randint(0,last)
+  print(quotes[rnd])
+  
 if __name__== "__main__":
   primary()
